1daa19385dbf1b2b277da78dd688a3c0ac8719f4
Golu jha <121981620+golujha9936@users.noreply.github.com>
2024-02-17 19:24:16 +0530

    Update autoleave.py

--- DAXXMUSIC/plugins/misc/autoleave.py
+++ DAXXMUSIC/plugins/misc/autoleave.py
@@ -30,8 +30,7 @@ async def auto_leave():
                             chat_id = i.chat.id
                             if (
                                 chat_id != config.LOGGER_ID
-                                and i.chat.id != -1001919135283
-                                and i.chat.id != -1001841879487
+                                and i.chat.id != -1002133369721
                             ):
                                 if left == 20:
                                     continue
