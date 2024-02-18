2b04bdd956d67eafe344f36075fdd23ac02ddd9c
Golu jha <121981620+golujha9936@users.noreply.github.com>
2024-02-18 09:54:42 +0530

    Update chatgpt.py

--- DAXXMUSIC/plugins/tools/chatgpt.py
+++ DAXXMUSIC/plugins/tools/chatgpt.py
@@ -36,7 +36,7 @@ async def chat(app :app, message):
 
 
 
-@app.on_message(filters.command(["aan" , ],  prefixes=["j","J"]))
+@app.on_message(filters.command(["aby" , ],  prefixes=["b","B"]))
 async def chat(app :app, message):
     
     try:
