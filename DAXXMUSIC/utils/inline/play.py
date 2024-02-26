1846e4c61367c8edd8b0e9561adf4aa9d5889b1a
Mr_cute_x <149322107+Anurag8953@users.noreply.github.com>
2024-02-26 16:02:24 +0530

    play.py

--- DAXXMUSIC/utils/inline/play.py
+++ DAXXMUSIC/utils/inline/play.py
@@ -70,7 +70,7 @@ def stream_markup_timer(_, chat_id, played, dur):
          InlineKeyboardButton(text="😈ᴏᴡɴᴇʀ😈", user_id=config.OWNER_ID),
          InlineKeyboardButton(text="✨sᴜᴘᴘᴏʀᴛ✨", url=f"{SUPPORT_CHAT}",),
         ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        [InlineKeyboardButton(text="•ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ•", url=f"https://t.me/Miss_ruhi_bot?startgroup=true")],
     ]
     return buttons
 
@@ -87,7 +87,7 @@ def stream_markup(_, chat_id):
          InlineKeyboardButton(text="😈ᴏᴡɴᴇʀ😈", user_id=config.OWNER_ID),
          InlineKeyboardButton(text="✨sᴜᴘᴘᴏʀᴛ✨", url=f"{SUPPORT_CHAT}",),
         ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        [InlineKeyboardButton(text="•ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ•", url=f"https://t.me/Miss_ruhi_bot?startgroup=true")],
     ]
     return buttons
 
