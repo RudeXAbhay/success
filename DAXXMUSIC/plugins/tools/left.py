de4c765db498e95d74fa0fe0d9ffc72ddc84cae4
RudeXAbhay <165001764+RudeXAbhay@users.noreply.github.com>
2024-04-02 10:18:36 -0700

    left.py

--- DAXXMUSIC/plugins/tools/left.py
+++ DAXXMUSIC/plugins/tools/left.py
@@ -92,7 +92,7 @@ async def member_has_left(client: app, member: ChatMemberUpdated):
                 profile_path=photo,
             )
         
-            caption = f"**#New_Member_Left**\n\n**๏** {user.mention} **ʜᴀs ʟᴇғᴛ ᴛʜɪs ɢʀᴏᴜᴘ**\n**๏ sᴇᴇ ʏᴏᴜ sᴏᴏɴ ᴀɢᴀɪɴ..!**\n│ \n└➻ ᴍᴀᴅᴇ ʙʏ ❣️✰ 𝕄𝕣 𝕔𝕦𝕥𝕖 𝕩 ✰😈"
+            caption = f"**#New_Member_Left**\n\n**๏** {user.mention} **ʜᴀs ʟᴇғᴛ ᴛʜɪs ɢʀᴏᴜᴘ**\n**๏ sᴇᴇ ʏᴏᴜ sᴏᴏɴ ᴀɢᴀɪɴ..!**\n│ \n└➻ ᴍᴀᴅᴇ ʙʏ [☠️ ᴛʜᴀᴋᴜʀ ᴀʙʜᴀʏ ☠️](https://t.me/rudexabhay)"
             button_text = "๏ ᴠɪᴇᴡ ᴜsᴇʀ ๏"
 
             # Generate a deep link to open the user's profile
